a725a07e5dc4ef8d1c0ab518867adbfd923546a6
Ofir <ofirelya2@gmail.com>
2022-06-11 17:01:42 +0300

    cpp and h files added

---
+++ Cards/Pitfall.cpp
@@ -0,0 +1,4 @@
+//
+// Created by ofir1 on 11-Jun-22.
+//
+
---
+++ Cards/Vampire.h
@@ -0,0 +1,8 @@
+//
+// Created by ofir1 on 11-Jun-22.
+//
+
+#ifndef CS234124_EX4_VAMPIRE_H
+#define CS234124_EX4_VAMPIRE_H
+
+#endif //CS234124_EX4_VAMPIRE_H
