ff8cb2ca1510c838ffb063584884a8bee8365b16
alofeld@gmail.com <Depo2mypro!@#>
2022-06-13 20:51:49 +0300

    pass valgrind custom made

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 
+g++ --std=c++11 -o mtmchkin -Wall -pedantic-errors -Werror -DNDEBUG test.cpp Mtmchkin.cpp Cards/*.cpp Players/*.cpp utilities.cpp
+valgrind --leak-check=full ./mtmchkin
 
 rm submission.zip
 zip -r submission.zip tool/main.cpp Mtmchkin.h Mtmchkin.cpp Cards Players dry.pdf Exception.h
