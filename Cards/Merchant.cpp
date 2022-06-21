5e2b509cbf317df434f2329ee27448750e552552
alofeld@gmail.com <Depo2mypro!@#>
2022-06-21 16:55:13 +0300

    fixing bug in merchant.cpp

--- Cards/Merchant.cpp
+++ Cards/Merchant.cpp
@@ -1,21 +1,48 @@
 //
 // Created by ofir1 on 12-Jun-22.
 //
-
+#include <string>
 #include "Merchant.h"
 
 Merchant::Merchant():
         Card()
 {}
 
+//bool checkMerchantInput(string raw){
+//    for (char const &c: raw) {
+//        if (!std::isdigit(c)) {
+//            return false;
+//        }
+//
+//    }
+//    if (raw.length()==0) {
+//        return false;
+//    }
+//    int Answer = std::stoi(raw);
+//    if ((Answer!=LEAVE) && (Answer!=BUY_HP) && (Answer!=BUY_FORCE))
+//        return false;
+//    return true;
+//}
 
 void Merchant::applyEncounter(Player& player) const{
     printMerchantInitialMessageForInteractiveEncounter(std::cout, player.getName(), player.getCoins());
     int Answer;
-//    std::cin >> Answer;
+    string RawAnswer;
 
     while((std::cin >> Answer).fail() || ((Answer!=LEAVE) && (Answer!=BUY_HP) && (Answer!=BUY_FORCE))) {
 //        bool check =;
+//        std::getline(std::cin , RawAnswer);
+//        if (RawAnswer.empty()){
+//            continue;
+//        }
+//        std::istringstream iss(RawAnswer);
+//        if ((!RawAnswer.empty()) ||(std::cin >> Answer).good() || !((Answer!=LEAVE) && (Answer!=BUY_HP) && (Answer!=BUY_FORCE))){
+//            break;
+//        }
+//        if (checkMerchantInput(RawAnswer)){
+//            Answer = std::stoi(RawAnswer);
+//            break;
+//        }
         printInvalidInput();
         std::cin.clear();
         std::cin.ignore(CHUNK_SIZE, '\n');
